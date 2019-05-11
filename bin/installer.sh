8c4511d886c7c59a514bcd057afa05090e44a49d
Bodo Schulz <bodo@boone-schulz.de>
2019-05-11 14:16:54 +0200

    more installer fixes

--- bin/installer.sh
+++ bin/installer.sh
@@ -26,34 +26,46 @@ do
   cp -a ${SOURCE_DIR}/${i} ${DESTINATION_DIR}/
 done
 
-if [[ -e /sbin/openrc-run ]]
+if [[ ! -e /etc/icinga2-cert-service.yaml ]]
 then
-  cat << EOF >> /etc/conf.d/icinga2-cert-service
+  cp config_example.yaml /etc/icinga2-cert-service.yaml
+fi
+
+
+if [[ $(command -v openrc-run | wc -l) -eq 1 ]]
+then
+  echo "install openrc init and configuration files"
+
+  cp ${SOURCE_DIR}/init-script/openrc/icinga2-cert-service /etc/init.d/
+
+  cat << EOF > /etc/conf.d/icinga2-cert-service
 
 # Icinga2 cert service
 CERT_SERVICE_BIN="/usr/local/icinga2-cert-service/bin/icinga2-cert-service.rb"
 
 EOF
-  cp ${SOURCE_DIR}/init-script/openrc/icinga2-cert-service /etc/init.d/
 fi
 
-if [[ -e /bin/systemctl ]]
+if [[ $(command -v systemctl | wc -l) -eq 1 ]]
 then
-  cp config_example.yaml /etc/icinga2-cert-service.yaml
+  echo "install system unit file"
 
-cat << EOF >>   /etc/systemd/system/icinga-cert-service.service
-[Unit]
-Description=starts the icinga certificate service
+  cp ${SOURCE_DIR}/init-script/systemd/icinga-cert-service.service /etc/systemd/system/
 
-[Service]
-Environment="LOG_LEVEL=INFO"
-Environment="CERT_SERVICE=/usr/local/icinga2-cert-service"
-Environment="CERT_SERVICE_BIN=/usr/local/icinga2-cert-service/bin/icinga2-cert-service.rb"
-ExecStart=/usr/bin/ruby /usr/local/icinga2-cert-service/bin/icinga2-cert-service.rb
-
-[Install]
-WantedBy=multi-user.target
-EOF
+#  cat << EOF > /etc/systemd/system/icinga-cert-service.service
+#[Unit]
+#Description=starts the icinga certificate service
+#
+#[Service]
+#Environment="LOG_LEVEL=INFO"
+#Environment="CERT_SERVICE=/usr/local/icinga2-cert-service"
+#Environment="CERT_SERVICE_BIN=/usr/local/icinga2-cert-service/bin/icinga2-cert-service.rb"
+#ExecStart=/usr/bin/ruby /usr/local/icinga2-cert-service/bin/icinga2-cert-service.rb
+#
+#[Install]
+#WantedBy=multi-user.target
+#
+#EOF
 
   systemctl daemon-reload
   systemctl enable icinga-cert-service
